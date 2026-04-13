2c570697cbbfc9eaf2697e656726873a45d701fd
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2026-04-12 23:10:28 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,35 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'TalentStream — Descubre Talentos',
   description: 'Mira, anima y sube a la cima. Streaming de talentos en vivo.',
-}
+
+  icons: {
+    icon: '/favicon.ico',
+  },
+
+  openGraph: {
+    title: 'TalentStream — Descubre Talentos',
+    description: 'Mira, anima y sube a la cima. Streaming de talentos en vivo.',
+    url: 'https://talento-fawn.vercel.app',
+    siteName: 'TalentStream',
+    images: [
+      {
+        url: '/images/preview.png',
+        width: 1200,
+        height: 630,
+        alt: 'TalentStream',
+      },
+    ],
+    locale: 'es_ES',
+    type: 'website',
+  },
+
+  twitter: {
+    card: 'summary_large_image',
+    title: 'TalentStream — Descubre Talentos',
+    description: 'Mira, anima y sube a la cima.',
+    images: ['/preview.png'],
+  },
+};
 
 export default function RootLayout({
   children,
